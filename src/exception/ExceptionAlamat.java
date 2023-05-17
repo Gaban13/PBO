322203f4e1affdbad34d9d73bb771b906a828bdb
Gaban13 <hagaisuranta@hotmail.com>
2023-05-17 17:10:23 +0700

    Update ExceptionAlamat.java

--- src/exception/ExceptionAlamat.java
+++ src/exception/ExceptionAlamat.java
@@ -5,5 +5,6 @@ package exception;
 public class ExceptionAlamat extends Exception {
     public void showMessage(){
         System.out.println("[!] alamat yang anda masukkan harus diawali dengan kata 'jln.' [!]");
+        //
     }
 }
